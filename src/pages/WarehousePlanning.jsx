36822fdcc412fd95bffc8b135ae5db4d0f7bdc31
Jeremy Traini <z5362885@ad.unsw.edu.au>
2023-04-12 12:16:35 +1000

    Stub content in warehouse planning

--- src/pages/WarehousePlanning.jsx
+++ src/pages/WarehousePlanning.jsx
@@ -2,7 +2,11 @@ import { BasicPage } from "./BasicPage";
 import Home from "@mui/icons-material/Home";
 
 const WarehousePlanning = () => {
-  return <BasicPage title="Warehouse Planning" icon={<Home />} />;
+  return (
+    <BasicPage title="Warehouse Planning" >
+      Hello world
+    </BasicPage>
+    );
 };
 
 export default WarehousePlanning;
