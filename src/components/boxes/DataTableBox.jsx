3dfac384b63d2f97179c879ad3221dc1a4c627da
Jeremy Traini <z5362885@ad.unsw.edu.au>
2023-04-12 14:10:35 +1000

    Changing datatables to builtin MUI tables

--- src/components/boxes/DataTableBox.jsx
+++ src/components/boxes/DataTableBox.jsx
@@ -2,17 +2,9 @@ import React from 'react';
 import MUIDataTable from "mui-datatables";
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { withStyles } from "@mui/styles";
-import { createMuiTheme } from "@mui/styles"
-
-const customTheme = createTheme({
-  overrides: {
-    MUIDataTable: {
-      paper: {
-        boxShadow: "none"
-      }
-    }
-  }
-});
+import { createMuiTheme } from "@mui/styles";
+import { DataGrid } from '@mui/x-data-grid';
+import Box from '@mui/material/Box';
 
 const DataTableBox = ({type, from_date, to_date}) => {
   let title = "Test";
@@ -26,81 +18,94 @@ const DataTableBox = ({type, from_date, to_date}) => {
       break;
   }
 
-  const options = {
-    selectableRows: false,
-    search: true,
-    download: false,
-    print: false,
-    viewColumns: false,
-    filter: true,
-    filterType: "dropdown",
-    responsive: "standard",
-    tableBodyHeight: "100%",
-    // onTableChange: (action, state) => {
-    //   console.log(action);
-    //   console.dir(state);
-    // },
-    setTableProps: () => {
-      return {
-        size: 'small',
-      };
-    },
-    
-  };
-
   const columns = [
     {
-     name: "name",
-     label: "Name",
-     options: {
-      filter: true,
-      sort: false,
-      fullWidth: true,
-     }
+      field: "name",
+      headerName: "Client",
+      type: 'string',
+      width: 120,
     },
     {
-     name: "total-deliveries",
-     label: "Total Deliveries",
-     options: {
-      filter: true,
-      sort: true,
-     }
+      field: "total-deliveries",
+      headerName: "Total Deliveries",
+      type: 'number',
+      width: 150,
     },
     {
-     name: "total-revenue",
-     label: "Total Revenue",
-     options: {
-      filter: true,
-      sort: true,
-     }
+      field: "total-revenue",
+      headerName: "Total Revenue",
+      type: 'number',
+      width: 150,
     },
    ];
 
-
-  const data = [
-    ["Jaden Collins", 10000, 10000],
-    ["Franky Rees", 10000, 10000],
-    ["Aaren Rose", null, 10000],
-    ["Johnny Jones", 10000, 10000],
-    ["Jimmy Johns", 10000, 10000],
-    ["Jack Jackson", 10000, 10000],
-    ["Joe Jones", 10000, 10000],
-    ["Jacky Jackson", 10000, 10000],
-    ["Jo Jo", 10000, 10000],
-    ["Donna Marie", 10000, 10000]
-  ];
-
-  
+  const rows = [
+    { id: 1, name: 'Jaden Collins', 'total-deliveries': 10000, 'total-revenue': 10000 },
+    { id: 2, name: 'Franky Rees', 'total-deliveries': 10000, 'total-revenue': 10000 },
+    { id: 3, name: 'Aaren Rose', 'total-deliveries': 100, 'total-revenue': 10000 },
+    { id: 4, name: 'Johnny Jones', 'total-deliveries': 10000, 'total-revenue': 10000 },
+    { id: 5, name: 'Jimmy Johns', 'total-deliveries': 10000, 'total-revenue': 10000 },
+    { id: 6, name: 'Jack Jackson', 'total-deliveries': 20000, 'total-revenue': 10000 },
+    { id: 7, name: 'Joe Jones', 'total-deliveries': 30000, 'total-revenue': 10000 },
+    { id: 8, name: 'Jacky Jackson', 'total-deliveries': 10000, 'total-revenue': 10000 },
+    { id: 9, name: 'Jo Jo', 'total-deliveries': 10000, 'total-revenue': 10000 },
+    { id: 10, name: 'Donna Marie', 'total-deliveries': 10000, 'total-revenue': 10000 },
+  ]
 
   return (
-    <ThemeProvider theme={customTheme}>
-      <MUIDataTable
-          title={title}
-          data={data}
+    <Box sx={{
+      margin: '10px 10px',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'space-between',
+      height: '100%',
+      textAlign: 'left',
+    }}>
+      <Box>
+        {title}
+      </Box>
+      <Box sx={{
+          flexGrow: 1,
+        }}>
+        <DataGrid
+          density="compact"
+          rows={rows}
           columns={columns}
-          options={options}
+          initialState={{
+              pagination: {
+                paginationModel: {
+                  pageSize: 5,
+                },
+              },
+              sorting: {
+                sortModel: [
+                  {
+                    field: 'total-deliveries',
+                    sort: 'desc',
+                  },
+                ],
+              },
+            }}
+          pageSize={5}
+          rowsPerPageOptions={[5]}
+          disableRowSelectionOnClick
+          hideFooterSelectedRowCount
+          hideFooterPagination
+          hideFooter
+          sx={{
+            '& .RaDatagrid-clickableRow': { cursor: 'default' },
+            // Remove inner borders
+            '& .MuiDataGrid-columnHeader, .MuiDataGrid-cell': {
+              borderRight: 'none',
+              borderBottom: 'none',
+            },
+            // Remove the outer borders
+            border: 0
+
+          }}
         />
-    </ThemeProvider>
+      </Box>
+    </Box>
   );
 };
 
---
+++ src/components/boxes/DataTableBox_old.jsx
@@ -0,0 +1,116 @@
+import React from 'react';
+import MUIDataTable from "mui-datatables";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { withStyles } from "@mui/styles";
+import { createMuiTheme } from "@mui/styles";
+import { DataGrid } from '@mui/x-data-grid';
+
+const customTheme = createTheme({
+  overrides: {
+    MUIDataTable: {
+      paper: {
+        boxShadow: "none"
+      }
+    }
+  }
+});
+
+const DataTableBox = ({type, from_date, to_date}) => {
+  let title = "Test";
+
+  switch (type) {
+    case "client":
+      title = "Data by client";
+      break;
+    case "suburb":
+      title = "Data by suburb";
+      break;
+  }
+
+  const options = {
+    selectableRows: false,
+    search: true,
+    download: false,
+    print: false,
+    viewColumns: false,
+    filter: true,
+    filterType: "dropdown",
+    responsive: "standard",
+    tableBodyHeight: "100%",
+    // onTableChange: (action, state) => {
+    //   console.log(action);
+    //   console.dir(state);
+    // },
+    setTableProps: () => {
+      return {
+        size: 'small',
+      };
+    },
+    
+  };
+
+  const columns = [
+    {
+     name: "name",
+     label: "Name",
+     options: {
+      filter: true,
+      sort: true,
+      fullWidth: true,
+     }
+    },
+    {
+     name: "total-deliveries",
+     label: "Total Deliveries",
+     options: {
+      filter: true,
+      sort: true,
+     }
+    },
+    {
+     name: "total-revenue",
+     label: "Total Revenue",
+     options: {
+      filter: true,
+      sort: true,
+     }
+    },
+   ];
+
+
+  const data = [
+    ["Jaden Collins", 10000, 10000],
+    ["Franky Rees", 10000, 10000],
+    ["Aaren Rose", null, 10000],
+    ["Johnny Jones", 10000, 10000],
+    ["Jimmy Johns", 10000, 10000],
+    ["Jack Jackson", 10000, 10000],
+    ["Joe Jones", 10000, 10000],
+    ["Jacky Jackson", 10000, 10000],
+    ["Jo Jo", 10000, 10000],
+    ["Donna Marie", 10000, 10000]
+  ];
+
+  
+
+  return (
+    // <ThemeProvider theme={customTheme}>
+    //   <MUIDataTable
+    //       title={title}
+    //       data={data}
+    //       columns={columns}
+    //       options={options}
+    //     />
+    // </ThemeProvider>
+    <DataGrid
+      rows={rows}
+      columns={columns}
+      pageSize={5}
+      rowsPerPageOptions={[5]}
+      checkboxSelection
+    />
+  );
+};
+
+export default (DataTableBox);
+
