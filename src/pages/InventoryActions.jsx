311465199e637a9e7036fbd7cccafb37729a3f27
Ahona Dutta <adbusiness047@gmail.com>
2023-04-19 00:22:26 +1000

    Updated with notifications and pop-ups sending notifications/invoices and more to relevant stakeholders

--- src/pages/InventoryActions.jsx
+++ src/pages/InventoryActions.jsx
@@ -1,12 +1,30 @@
 import { BasicPage } from "./BasicPage";
-import React from 'react';
+import React, { useState } from 'react';
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Checkbox from '@mui/material/Checkbox';
 import FormControlLabel from '@mui/material/FormControlLabel';
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
+
 
 const InventoryActions = () => {
+  const [alerts, setAlerts] = useState([]);
+
+  const handleClick = (message, severity) => {
+    const id = Date.now();
+    const newAlert = {
+      id,
+      message,
+      severity
+    };
+    setAlerts((prevAlerts) => [...prevAlerts, newAlert]); // Add new alert to the alerts array
+  };
+
+  const handleClose = (id) => {
+    setAlerts((prevAlerts) => prevAlerts.filter((alert) => alert.id !== id)); // Remove alert from the alerts array
+  };
   
   return <BasicPage>
       <h1 style={{ color: 'black' }}>Inventory Actions</h1>
@@ -33,21 +51,26 @@ const InventoryActions = () => {
           alignItems="flex-end"
           sx={{ height: 20 }}
         >
-          <Button variant="contained" color="success" sx={{ height: 30, width: 200 }}>
+          <div>
+          <Button variant="contained" color="success" sx={{ height: 30, width: 200 }}onClick = {() => handleClick('Sent notification to Churros West warehouse to deliver 1000 units of Pens to Metro Warehouse', 'success')}>
             Notify Warehouse
           </Button>
-          {/* <Popover
-            id={id}
-            open={open}
-            anchorEl={anchorEl}
-            onClose={handleClose}
-            anchorOrigin={{
-              vertical: 'bottom',
-              horizontal: 'left',
-            }}
-          >
-            <Typography sx={{ p: 2 }}>The content of the Popover.</Typography>
-          </Popover> */}
+            {alerts.map((alert) => (
+              <Snackbar
+                key={alert.id}
+                open
+                autoHideDuration={6000}
+                onClose={() => handleClose(alert.id)}
+                anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+              >
+                <MuiAlert elevation={6} variant="filled" onClose={() => handleClose(alert.id)} severity={alert.severity}>
+                  {alert.message}
+                </MuiAlert>
+              </Snackbar>
+            ))}
+          </div>
+
+          
         </Box>
       </Container>
 
@@ -60,9 +83,8 @@ const InventoryActions = () => {
                 value="SomeValue"
             />
         }
-        label="Sent overdue payment reminder to Officeworks for payment over due by {3 days, due date on 13/03/2023}"/>
+        label="Send overdue payment reminder to Officeworks for payment of $1759.55. "/>
         <p>Reason: Payment was due on 13/03/2023, which was 3 days ago. Failure to pay will mean product delivery will be cancelled.</p>
-        
         <Box
           m={1}
         //margin
@@ -71,10 +93,28 @@ const InventoryActions = () => {
           alignItems="flex-end"
           sx={{ height: 20 }}
         >
-          <Button variant="contained" color="warning" sx={{ height: 45, width: 200 }} >
-            Send Overdue Payment Notice
+          <div>
+          <Button variant="contained" color="error" sx={{ height: 45, width: 200 }}onClick = {() => handleClick('Sent overdue payment reminder to Officeworks for payment of $1759.55.', 'error')}>
+            Overdue Payment Reminder Notice
           </Button>
+            {alerts.map((alert) => (
+              <Snackbar
+                key={alert.id}
+                open
+                autoHideDuration={6000}
+                onClose={() => handleClose(alert.id)}
+                anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+              >
+                <MuiAlert elevation={6} variant="filled" onClose={() => handleClose(alert.id)} severity={alert.severity}>
+                  {alert.message}
+                </MuiAlert>
+              </Snackbar>
+            ))}
+          </div>
+
+          
         </Box>
+        
       </Container>
 
       <h2>Churro's West Warehouse</h2>
@@ -89,7 +129,7 @@ const InventoryActions = () => {
                   value="SomeValue"
               />
           }
-          label="Expected payment for 10,000 rubbers from OfficeGo from Metro Warehouse to West Warehouse"/>
+          label="Expected payment for 10,000 rubbers from OfficeGo with scheduled pickup from East Warehouse"/>
           <p>Reason: Upcoming scheduled payment on a weekly delivery for OfficeGo - 10,000 units delivered every Wednesday.</p>
           
           <Box
@@ -100,9 +140,24 @@ const InventoryActions = () => {
             alignItems="flex-end"
             sx={{ height: 20 }}
           >
-            <Button variant="contained" color="warning" sx={{ height: 30, width: 200 }} >
-              Send Invoice
-            </Button>
+          <div>
+          <Button variant="contained" color="warning" sx={{ height: 30, width: 200 }}onClick = {() => handleClick('Sent regular payment invoice for 10,000 rubbers from OfficeGo with pickup at East Warehouse', 'warning')}>
+            Send Invoice
+          </Button>
+            {alerts.map((alert) => (
+              <Snackbar
+                key={alert.id}
+                open
+                autoHideDuration={6000}
+                onClose={() => handleClose(alert.id)}
+                anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+              >
+                <MuiAlert elevation={6} variant="filled" onClose={() => handleClose(alert.id)} severity={alert.severity}>
+                  {alert.message}
+                </MuiAlert>
+              </Snackbar>
+            ))}
+          </div>
 
             
           </Box>
@@ -128,9 +183,25 @@ const InventoryActions = () => {
         alignItems="flex-end"
         sx={{ height: 20 }}
       >
-        <Button variant="contained" color="success" sx={{ height: 30, width: 200 }}>
-          Notify Warehouse
-        </Button>
+        
+        <div>
+          <Button variant="contained" color="success" sx={{ height: 30, width: 200 }}onClick = {() => handleClick('Sent notification to Churros South warehouse to deliver 7000 units of Tote Bags to West Warehouse', 'success')}>
+            Notify Warehouse
+          </Button>
+            {alerts.map((alert) => (
+              <Snackbar
+                key={alert.id}
+                open
+                autoHideDuration={6000}
+                onClose={() => handleClose(alert.id)}
+                anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+              >
+                <MuiAlert elevation={6} variant="filled" onClose={() => handleClose(alert.id)} severity={alert.severity}>
+                  {alert.message}
+                </MuiAlert>
+              </Snackbar>
+            ))}
+          </div>
       </Box>
     </Container>
       
@@ -144,8 +215,8 @@ const InventoryActions = () => {
               value="SomeValue"
           />
       }
-      label="Expected payment for 10,000 rubbers from OfficeGo from Metro Warehouse to West Warehouse"/>
-      <p>Reason: Upcoming scheduled payment on a weekly delivery for OfficeGo - 10,000 units delivered every Wednesday.</p>
+      label="Upcoming payment for 850,000 ink refills for Uniball with pickup at East Warehouse"/>
+      <p>Reason: ADHOC request from Uniball for a big product launch - requested pick up at East Warehouse and potential delivery.</p>
       
       <Box
         m={1}
@@ -155,9 +226,24 @@ const InventoryActions = () => {
         alignItems="flex-end"
         sx={{ height: 20 }}
       >
-        <Button variant="contained" color="warning" sx={{ height: 30, width: 200 }} >
-          Send Invoice
-        </Button>
+        <div>
+          <Button variant="contained" color="warning" sx={{ height: 30, width: 200 }}onClick = {() => handleClick('Sent payment invoice for 850,000 ink refills for Uniball with pickup at East Warehouse', 'warning')}>
+            Send Invoice
+          </Button>
+            {alerts.map((alert) => (
+              <Snackbar
+                key={alert.id}
+                open
+                autoHideDuration={6000}
+                onClose={() => handleClose(alert.id)}
+                anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+              >
+                <MuiAlert elevation={6} variant="filled" onClose={() => handleClose(alert.id)} severity={alert.severity}>
+                  {alert.message}
+                </MuiAlert>
+              </Snackbar>
+            ))}
+          </div>
 
         
       </Box>
@@ -172,7 +258,7 @@ const InventoryActions = () => {
               value="SomeValue"
           />
       }
-      label="Move 750 units of notebooks from East Warehouse to this warehouse"/>
+      label="Move 750 units of notebooks from North Warehouse to this warehouse"/>
       <p>Reason: Your notebook sales in Revesby spiked 72% in the 6 days and 1500 units are needed by 2023-12-16.</p>
       
       <Box
@@ -183,9 +269,24 @@ const InventoryActions = () => {
         alignItems="flex-end"
         sx={{ height: 20 }}
       >
-        <Button variant="contained" color="success" sx={{ height: 30, width: 200 }}>
-          Notify Warehouse
-        </Button>
+        <div>
+          <Button variant="contained" color="success" sx={{ height: 30, width: 200 }}onClick = {() => handleClick('Sent notification to Churros North warehouse to deliver 750 units of Notebooks to East Warehouse', 'success')}>
+            Notify Warehouse
+          </Button>
+            {alerts.map((alert) => (
+              <Snackbar
+                key={alert.id}
+                open
+                autoHideDuration={6000}
+                onClose={() => handleClose(alert.id)}
+                anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+              >
+                <MuiAlert elevation={6} variant="filled" onClose={() => handleClose(alert.id)} severity={alert.severity}>
+                  {alert.message}
+                </MuiAlert>
+              </Snackbar>
+            ))}
+          </div>
       </Box>
     </Container>
     </BasicPage>
