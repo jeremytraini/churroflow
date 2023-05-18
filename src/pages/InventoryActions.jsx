b2d7cdcdf5649839f2ffe7cf445c9804b9d5aa10
Jeremy Traini <z5362885@ad.unsw.edu.au>
2023-05-19 09:00:35 +1000

    Fixes

--- src/pages/InventoryActions.jsx
+++ src/pages/InventoryActions.jsx
@@ -31,7 +31,7 @@ const InventoryActions = () => {
   
   return <BasicPage title="Inventory Actions">
       <BlurredBox type="Inventory Actions page" isBlurred={user.tier === 'Starter'}>
-        <h2 style={{ color: 'black', fontSize: '1.5em', marginTop: '1em' }}>Churro's Metro Warehouse</h2>
+        <h2 style={{ color: 'black', fontSize: '1.5em', marginTop: '1em' }}>Churros West Warehouse</h2>
 
         
         <Container maxWidth="lg" maxHeight = "md" style={{ background: '#ffffff' }}>
@@ -44,7 +44,7 @@ const InventoryActions = () => {
               />
           }
           label="Move 1000 units of Pens from West Warehouse to this warehouse"/>
-          <p>Reason: Your Pen sales in Kensington spiked 32% in the 3 days and 1000 units are needed by 2023-04-08.</p>
+          <p>Reason: Your Pen sales in Kensington spiked 32% in the 3 days and 1000 units are needed by 2023-05-22.</p>
           
           <Box
             m={1}
@@ -55,7 +55,7 @@ const InventoryActions = () => {
             sx={{ height: 20 }}
           >
             <div>
-            <Button variant="contained" color="success" sx={{ height: 30, width: 200 }} onClick = {() => handleClick('Sent notification to Churros West warehouse to deliver 1000 units of Pens to Metro Warehouse', 'success')}>
+            <Button variant="contained" color="success" sx={{ height: 30, width: 200 }} onClick = {() => handleClick('Sent notification to Churros West warehouse to deliver 1000 units of Pens to Churros West Warehouse', 'success')}>
               Notify Warehouse
             </Button>
               {alerts.map((alert) => (
@@ -87,7 +87,7 @@ const InventoryActions = () => {
               />
           }
           label="Send overdue payment reminder to Officeworks for payment of $1759.55. "/>
-          <p>Reason: Payment was due on 13/03/2023, which was 3 days ago. Failure to pay will mean product delivery will be cancelled.</p>
+          <p>Reason: Payment was due on 2023-05-16, which was 3 days ago. Failure to pay will mean product delivery will be cancelled.</p>
           <Box
             m={1}
           //margin
@@ -120,7 +120,7 @@ const InventoryActions = () => {
           
         </Container>
 
-        <h2 style={{ color: 'black', fontSize: '1.5em', marginTop: '1em' }}>Churro's West Warehouse</h2>
+        <h2 style={{ color: 'black', fontSize: '1.5em', marginTop: '1em' }}>Churros West Warehouse</h2>
 
 
         <Container maxWidth="lg" maxHeight = "sm" style={{ background: '#ffffff' }}>
@@ -132,7 +132,7 @@ const InventoryActions = () => {
                     value="SomeValue"
                 />
             }
-            label="Expected payment for 10,000 rubbers from OfficeGo with scheduled pickup from East Warehouse"/>
+            label="Expected payment for 10,000 rubbers from OfficeGo with scheduled pickup from Inner-West Warehouse"/>
             <p>Reason: Upcoming scheduled payment on a weekly delivery for OfficeGo - 10,000 units delivered every Wednesday.</p>
             
             <Box
@@ -144,7 +144,7 @@ const InventoryActions = () => {
               sx={{ height: 20 }}
             >
             <div>
-            <Button variant="contained" color="warning" sx={{ height: 30, width: 200 }} onClick = {() => handleClick('Sent regular payment invoice for 10,000 rubbers from OfficeGo with pickup at East Warehouse', 'warning')}>
+            <Button variant="contained" color="warning" sx={{ height: 30, width: 200 }} onClick = {() => handleClick('Sent regular payment invoice for 10,000 rubbers from OfficeGo with pickup at Churros Inner-West Warehouse', 'warning')}>
               Send Invoice
             </Button>
               {alerts.map((alert) => (
@@ -176,7 +176,7 @@ const InventoryActions = () => {
             />
         }
         label="Move 7000 units of tote bags from South Warehouse to this warehouse"/>
-        <p>Reason: Your Tote Bag sales in Strathfield spiked 92% in the 4 days and an additional 2500 units are needed by 2023-05-02.</p>
+        <p>Reason: Your Tote Bag sales in Strathfield spiked 92% in the last 4 days and an additional 2500 units are needed by 2023-05-23.</p>
         
         <Box
           m={1}
@@ -208,7 +208,7 @@ const InventoryActions = () => {
         </Box>
       </Container>
         
-      <h2 style={{ color: 'black', fontSize: '1.5em', marginTop: '1em' }}>Churro's East Warehouse</h2>
+      <h2 style={{ color: 'black', fontSize: '1.5em', marginTop: '1em' }}>Churros Inner-West Warehouse</h2>
       <Container maxWidth="lg" maxHeight = "md" style={{ background: '#ffffff' }}>
 
       <FormControlLabel
@@ -218,8 +218,8 @@ const InventoryActions = () => {
                 value="SomeValue"
             />
         }
-        label="Upcoming payment for 850,000 ink refills for Uniball with pickup at East Warehouse"/>
-        <p>Reason: ADHOC request from Uniball for a big product launch - requested pick up at East Warehouse and potential delivery.</p>
+        label="Upcoming payment for 850,000 ink refills for Uniball with pickup at Inner-West Warehouse"/>
+        <p>Reason: ADHOC request from Uniball for a big product launch - requested pick up at Inner-West Warehouse and potential delivery.</p>
         
         <Box
           m={1}
@@ -230,7 +230,7 @@ const InventoryActions = () => {
           sx={{ height: 20 }}
         >
           <div>
-            <Button variant="contained" color="warning" sx={{ height: 30, width: 200 }} onClick = {() => handleClick('Sent payment invoice for 850,000 ink refills for Uniball with pickup at East Warehouse', 'warning')}>
+            <Button variant="contained" color="warning" sx={{ height: 30, width: 200 }} onClick = {() => handleClick('Sent payment invoice for 850,000 ink refills for Uniball with pickup at Inner-West Warehouse', 'warning')}>
               Send Invoice
             </Button>
               {alerts.map((alert) => (
@@ -261,8 +261,8 @@ const InventoryActions = () => {
                 value="SomeValue"
             />
         }
-        label="Move 750 units of notebooks from North Warehouse to this warehouse"/>
-        <p>Reason: Your notebook sales in Revesby spiked 72% in the 6 days and 1500 units are needed by 2023-12-16.</p>
+        label="Move 750 units of notebooks from South Warehouse to this warehouse"/>
+        <p>Reason: Your notebook sales in Revesby spiked 72% in the last 6 days and 1500 units are needed by 2023-5-27.</p>
         
         <Box
           m={1}
@@ -273,7 +273,7 @@ const InventoryActions = () => {
           sx={{ height: 20 }}
         >
           <div>
-            <Button variant="contained" color="success" sx={{ height: 30, width: 200 }} onClick = {() => handleClick('Sent notification to Churros North warehouse to deliver 750 units of Notebooks to East Warehouse', 'success')}>
+            <Button variant="contained" color="success" sx={{ height: 30, width: 200 }} onClick = {() => handleClick('Sent notification to Churros South warehouse to deliver 750 units of Notebooks to Inner-West Warehouse', 'success')}>
               Notify Warehouse
             </Button>
               {alerts.map((alert) => (
